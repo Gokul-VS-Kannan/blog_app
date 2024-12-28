a110f67b70bffa9995d52266c197fb13d89fab7f
Gokul-VS-Kannan <gokulvsvikram@gmail.com>
2024-12-28 12:41:00 +0530

    Added some views and html files

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -8,4 +8,7 @@ from . import views
 urlpatterns=[
     path('',views.home,name='home'),
     path('register',views.register,name='register'),
+    path('login_view',views.login_view,name='login_view'),
+    path('logout_view',views.logout_view,name='logout_view'),
+    path('welcome',views.welcome,name='welcome'),
 ]
--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -84,3 +85,8 @@ def register(request):
         return redirect('home')
 
     return render(request, 'registration.html')
+
+# a simple function for displaying welcome page after login
+@login_required
+def welcome(request):
+    return render(request,'loginhome.html')

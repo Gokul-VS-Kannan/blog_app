3f701122d8d6c9b67ce9a33f7ac1a2021f95e962
Gokul-VS-Kannan <gokulvsvikram@gmail.com>
2024-12-31 16:19:31 +0530

    Add models

--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+
+
+# create models for blog
+
+class BlogPost(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,default=None,related_name='user_blog')
+    title=models.CharField(max_length=200)
+    content=models.TextField()
+    created_on=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    # get_user_full_name method will combine first_name and last_name in user table
+    def get_user_full_name(self):
+        return self.user.get_full_name()  
+    
+    def get_user_username(self):
+        return self.user.username 
